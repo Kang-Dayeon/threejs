21151ea0cc1aff1adea565b9c0196b27a6ad5b16
kang-dayeon <dksldiss89@gmail.com>
2022-09-21 18:30:51 +0900

    orthographic camera 테스트

--- js/main.js
+++ js/main.js
@@ -18,6 +18,7 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 const scene = new THREE.Scene();
 
 // camera : 카메라
+// perspective camera : 사람눈으로 보는 것과 같이 요소의 원근감이 있음 (제일 많이 사용)
 const camera = new THREE.PerspectiveCamera(
     75, //시야각(field of view)
     window.innerWidth / window.innerHeight, // 종횡비(aspect)
@@ -28,6 +29,22 @@ const camera = new THREE.PerspectiveCamera(
 camera.position.x = 1;
 camera.position.y = 2;
 camera.position.z = 5;
+
+// orthographic camera : 게임에서 많이 사용됨(롤이나 디아블로등..) 거리와 상관없이 요소크기 똑같음
+// const camera = new THREE.OrthographicCamera(
+//     -(window.innerWidth / window.innerHeight), // left
+//     window.innerWidth / window.innerHeight, // right
+//     1, // top
+//     -1, // bottom
+//     0.1,
+//     1000
+// );
+// camera.position.x = 1;
+// camera.position.y = 2;
+// camera.position.z = 5;
+// camera.lookAt(0,0,0);
+// camera.zoom = 0.5;
+// camera.updateProjectionMatrix();
 
 // scene(무대)에 카메라를 추가해준는것
 scene.add(camera);
