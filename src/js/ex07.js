9bcfb5898d539a3de010373dee32f100ad2753de
kangdayeon <dykang@ui-lab.co.kr>
2022-09-23 13:58:04 +0900

    taterial 텍스쳐 각각 다른 이미지 넣기

--- src/js/ex07.js
+++ src/js/ex07.js
@@ -4,18 +4,28 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 export default function example() {
   // 텍스쳐 로드
   const textureLoder = new THREE.TextureLoader();
-  const textureImage = textureLoder.load(
-    '././textures/Brick_Wall_016_ambientOcclusion.jpg',
-    () => {
-      console.log('로드완료');
-    },
-    () => {
-      console.log('로드 중');
-    },
-    () => {
-      console.log('로드에러');
-    }
-  );
+  const rightTexture = textureLoder.load('./../textures/mcstyle/right.png');
+  const leftTexture = textureLoder.load('./../textures/mcstyle/left.png');
+  const topTexture = textureLoder.load('./../textures/mcstyle/top.png');
+  const bottomTexture = textureLoder.load('./../textures/mcstyle/bottom.png');
+  const backTexture = textureLoder.load('./../textures/mcstyle/back.png');
+  const frontTexture = textureLoder.load('./../textures/mcstyle/front.png');
+
+  const materials = [
+    new THREE.MeshBasicMaterial({ map: rightTexture }),
+    new THREE.MeshBasicMaterial({ map: leftTexture }),
+    new THREE.MeshBasicMaterial({ map: topTexture }),
+    new THREE.MeshBasicMaterial({ map: bottomTexture }),
+    new THREE.MeshBasicMaterial({ map: backTexture }),
+    new THREE.MeshBasicMaterial({ map: frontTexture }),
+  ];
+
+  rightTexture.magFilter = THREE.NearestFilter;
+  leftTexture.magFilter = THREE.NearestFilter;
+  topTexture.magFilter = THREE.NearestFilter;
+  bottomTexture.magFilter = THREE.NearestFilter;
+  backTexture.magFilter = THREE.NearestFilter;
+  frontTexture.magFilter = THREE.NearestFilter;
 
   const canvas = document.getElementById('three-canvas');
   const renderer = new THREE.WebGLRenderer({
@@ -38,7 +48,7 @@ export default function example() {
     1000 // fal
   );
 
-  camera.position.z = 8;
+  camera.position.z = 5;
 
   // scene(무대)에 카메라를 추가해준는것
   scene.add(camera);
@@ -54,12 +64,12 @@ export default function example() {
   const controls = new OrbitControls(camera, renderer.domElement);
 
   // mesh만들기
-  const geometry = new THREE.CylinderGeometry(1, 1, 5, 32);
-  const material = new THREE.MeshStandardMaterial({
-    color: '#ff0000',
-    map: textureImage
-  });
-  const mesh = new THREE.Mesh(geometry, material);
+  const geometry = new THREE.BoxGeometry(1, 1, 1);
+  // const material = new THREE.MeshBasicMaterial({
+  //   // color: '#ff0000',
+  //   map: topTexture
+  // });
+  const mesh = new THREE.Mesh(geometry, materials);
   scene.add(mesh);
 
   renderer.render(scene, camera);
--- src/js/main.js
+++ src/js/main.js
@@ -10,6 +10,7 @@
 // import example from "./ex05.js";
 // 카메라 조정
 // import example from "./ex06.js";
+// material 텍스쳐 여러개 넣기(마인크레프트 블럭만들기)
 import example from "./ex07.js";
 
 example();
